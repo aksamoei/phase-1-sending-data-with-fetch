5749301be7b841a3c4af729d6550a96a049e25d6
Abraham <abukip344@gmail.com>
2024-04-03 01:57:20 -0400

    sending data

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(username, useremail){
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+                },
+        body: JSON.stringify({
+            name: username,
+            email: useremail
+        })
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(userData){
+       const idParagrapgh = document.createElement("p");
+       idParagrapgh.textContent = userData.id;
+       document.querySelector("body").appendChild(idParagrapgh);
+    })
+    .catch(function(error){
+        const errorParagraph = document.createElement("p");
+        errorParagraph.textContent = error.message;
+        document.querySelector("body").appendChild(errorParagraph);
+    })
+}
+
+submitData();
+
+
+
